3bcda7fc98de652630d27feded69bcf09c5aa130
Joesph O'Brien <98370624+89jobrien@users.noreply.github.com>
2026-03-16 16:31:52 -0500

    ops: install minibox CLI with systemd setup

--- ops/install-systemd.sh
+++ ops/install-systemd.sh
@@ -3,12 +3,15 @@ set -euo pipefail
 
 BIN_SRC="${BIN_SRC:-./target/release/miniboxd}"
 BIN_DST="${BIN_DST:-/usr/local/bin/miniboxd}"
+CLI_SRC="${CLI_SRC:-./target/release/minibox}"
+CLI_DST="${CLI_DST:-/usr/local/bin/minibox}"
 UNIT_SRC="${UNIT_SRC:-./ops/miniboxd.service}"
 UNIT_DST="/etc/systemd/system/miniboxd.service"
 TMPFILES_SRC="${TMPFILES_SRC:-./ops/miniboxd.tmpfiles.conf}"
 TMPFILES_DST="/etc/tmpfiles.d/miniboxd.conf"
 
 install -m 0755 "$BIN_SRC" "$BIN_DST"
+install -m 0755 "$CLI_SRC" "$CLI_DST"
 install -m 0644 "$UNIT_SRC" "$UNIT_DST"
 install -m 0644 "$TMPFILES_SRC" "$TMPFILES_DST"
 
